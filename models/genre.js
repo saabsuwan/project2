b450fa6a6c653f0ae683e39eeca38a95abeb903f
Cedar <alexianfireflies@gmail.com>
2019-11-10 20:13:04 -0800

    added post route in html and fixed a few bugs

--- models/genre.js
+++ models/genre.js
@@ -1,17 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
     var Song = sequelize.define("Song", {
-    genre_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    song: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    artist: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
+        genre_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        song: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        artist: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     });
-    return Song;
 
+    return Song;
+}
