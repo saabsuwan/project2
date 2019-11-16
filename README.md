513f2f4c24f3ebec2c70ad9a43d8f008efb96c3e
Mariana <garman.mariana@gmail.com>
2019-11-16 12:03:37 -0800

    last changes

--- README.md
+++ README.md
@@ -6,7 +6,17 @@ App that lets you select your fitness activity from a list of 12, and returns a 
 
 ## Installation
 
-Using Git Bash or Terminal, clone the code on to your computer using `git clone https://github.com/saabsuwan/project2.git`. Following that, navigate to the folder the code is in and type `npm install` into the command line. Next, type `node server.js` to start the server. Then, open the seeds.sql file in MySQL and run it to populate the now-existing database, stop the server running, and start it again. With that, the website should be ready to go, so open up "http://localhost:8080".
+Using Git Bash or Terminal, clone the code on to your computer using `git clone https://github.com/saabsuwan/project2.git`. Following that, navigate to the folder the code is in and type `npm install` into the command line. After that, make a .env file and put 
+"DB_USER_LOCAL=root
+DB_PASSWORD_LOCAL=(your SQL password here)
+DB_DATABASE_LOCAL=playlist_db
+DB_HOST_LOCAL=localhost
+DB_USER_TEST=root
+DB_PASSWORD_TEST=(your SQL password here)
+DB_DATABASE_TEST=playlist_db
+DB_HOST_TEST=localhost"
+
+Next, type `node server.js` to start the server. Then, open the seeds.sql file in MySQL and run it to populate the now-existing database, stop the server running, and start it again. With that, the website should be ready to go, so open up "http://localhost:8080".
 
 ## Usage
 
@@ -30,3 +40,11 @@ App link:
 - HTML
 - Bootstrap
 - SASS
+
+## Future Features
+
+A list of features we'd like to add in the future:
+- Spotify API to load links to the songs you get in your playlist
+- a display of the genres we've associated with your chosen activity, and the option to exclude certain genres from the generated playlist if you really dislike one of those genres
+- attempt to use the Sequelize random function, because our current setup really only works as long as the database is relatively small
+- make the playlist on the page clear out if the person goes back to the default option in the list
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,73 +1,79 @@
+// require models and sequelize
 var db = require("../models");
 var Sequelize = require("sequelize");
 var sequelize = require("../config/config.js");
 
+// associations, to associate a particular activity with certain music genres
+// I did this to avoid having to deal with a bunch of if-else statements
 var associations = [
-  { activity: "zumba", genres: ["Latin", "Dancehall", "Salsa", "Merengue"] },
+  {activity: "zumba",
+  genres: ["Latin", "Dancehall", "Salsa", "Merengue"]},
 
-  {
-    activity: "running1",
-    genres: ["Progressive Rock", "Hip Hop", "Dance-Eletronic", "Pop"]
-  },
+  {activity: "running1",
+  genres: ["Progressive Rock", "Hip Hop", "Dance-Eletronic", "Pop"]},
 
-  {
-    activity: "running2",
-    genres: ["Heavy Rock", "Upbeat Pop", "Folk & Acoustic", "Indie Pop"]
-  },
+  {activity: "running2",
+  genres: ["Heavy Rock", "Upbeat Pop", "Folk & Acoustic", "Indie Pop"]},
 
-  {
-    activity: "stairmaster",
-    genres: ["Heavy Rock", "Upbeat Pop", "Folk & Acoustic", "Indie Pop"]
-  },
+  {activity: "stairmaster",
+  genres: ["Heavy Rock", "Upbeat Pop", "Folk & Acoustic", "Indie Pop"]},
 
-  { activity: "HIIT", genres: ["Electronic", "Rock", "Pop", "Hip Hop"] },
+  {activity: "HIIT",
+  genres: ["Electronic", "Rock", "Pop", "Hip Hop"]},
 
-  {
-    activity: "weightlifting",
-    genres: ["Metal", "Heavy Rock", "Indie Pop", "Electronic"]
-  },
+  {activity: "weightlifting",
+  genres: ["Metal", "Heavy Rock", "Indie Pop", "Electronic"]},
 
-  { activity: "yoga", genres: ["Mellow", "R&B", "Trip-Hop", "Chill"] },
+  {activity: "yoga",
+  genres: ["Mellow", "R&B", "Trip-Hop", "Chill"]},
 
-  { activity: "stretching", genres: ["Mellow", "R&B", "Trip-Hop", "Chill"] },
+  {activity: "stretching",
+  genres: ["Mellow", "R&B", "Trip-Hop", "Chill"]},
 
-  { activity: "rowing", genres: ["Metal", "Pop", "Hip-Hop", "Heavy Rock"] },
+  {activity: "rowing",
+  genres: ["Metal", "Pop", "Hip-Hop", "Heavy Rock"]},
 
-  { activity: "biking", genres: ["Electronic", "Pop", "Punk", "Reggae"] },
+  {activity: "biking",
+  genres: ["Electronic", "Pop", "Punk", "Reggae"]},
 
-  { activity: "hiking", genres: ["Punk", "World Music", "Rock", "Reggae"] },
+  {activity: "hiking",
+  genres: ["Punk", "World Music", "Rock", "Reggae"]},
 
-  {
-    activity: "aerobics",
-    genres: ["Pop", "Hip-Hop", "Dance-Electronic", "Latin"]
-  }
+  {activity: "aerobics",
+  genres: ["Pop", "Hip-Hop", "Dance-Electronic", "Latin"]}
 ];
 
+// export app so the post route makes it to the server
 module.exports = function(app) {
   app.post("/api/music", function(req, res) {
-    // console.log(req.body);
-    var reqActivity = req.body.activity;
-    // console.log(reqActivity);
+    // console.log(req.body); commented out, used for testing
 
+    // get the activity from the html
+    var reqActivity = req.body.activity;
+    // console.log(reqActivity); commented out, used for testing
+
+    // loop through the associations array until I find the object that matches the selected activity
     for (i = 0; i < associations.length; i++) {
-      // console.log("Hello!");
+      // console.log("Hello!"); commented out, used for testing
       if (associations[i].activity == reqActivity) {
-        // console.log("Hi there!");
+        // console.log("Hi there!"); commented out, used for testing
+
+        // get the music genres associated with the activity
         var musicGenres = associations[i].genres;
-        // console.log(musicGenres);
+        // console.log(musicGenres); commented out, used for testing
       }
     }
-    // order: Sequelize.fn("RAND"), limit: 10,
 
-    db.Song.findAll({
-      where: { genre_name: { [Sequelize.Op.or]: musicGenres } }
-    }).then(function(playlist) {
+    // find all the songs of the selected genres in our SQL database
+    db.Song.findAll({where: {genre_name: {[Sequelize.Op.or]: musicGenres}} }).then(function(playlist) {
       // console.log(playlist);
       var list = [];
 
+      // this returns a randomized list of 10 from the songs found in the database
       for (var i = 0; i < 10; i++) {
-        list.push(playlist[i]);
-        console.log(list);
+        var num = Math.floor(Math.random() * playlist.length);
+        list.push(playlist[num]);
+        // console.log(list); commented out, used for testing
       }
       res.json(list);
     });
